8ba10c596744ff87730eb6eb59cf69c250cd880c
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-10-26 20:41:05 +0530

    Update hastag.py

--- plugins/hastag.py
+++ plugins/hastag.py
@@ -15,7 +15,7 @@ async def hastag(bot, message):
     await message.reply_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ  ʜᴀsᴛᴀɢ :\n<pre>{res}</pre>", quote=True)
 
 
-__MODULE__ = "Hᴀsʜᴛᴀɢ"
+__MODULE__ = "🍷 𝐇𖽖𖾗𖽻𖾓𖽖ɢ 😻"
 __HELP__ = """
 **ʜᴀsʜᴛᴀɢ ɢᴇɴᴇʀᴀᴛᴏʀ:**
 
